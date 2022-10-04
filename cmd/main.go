df9d66d405c26b9a4e3c4f4fb5d4ec85fad4091e
damilola olayinka <holayinkajr@gmail.com>
2022-10-04 23:11:23 +0100

    push
    Signed-off-by: damilola olayinka <holayinkajr@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,6 @@ var (
 )
 
 func main() {
-	// clear flags initialized in static dependencies
 	if flag.CommandLine.Lookup("log_dir") != nil {
 		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	}
@@ -23,31 +22,8 @@ func main() {
 	var loggingFormat string
 	flag.StringVar(&loggingFormat, "loggingFormat", logging.TextFormat, "This flag sets the logging mode for klog")
 
-	// if err := flag.Set("v", "2"); err != nil {
-	// 	// fmt.Printf("failed to set log level: %s", err.Error())
-	// 	if errors.Is(err, fmt.Errorf("no such flag -v")) {
-	// 		fmt.Println("yo")
-	// 	} else {
-	// 		fmt.Println("yayyyyyyy")
-	// 	}
-
-	// }
 	flag.Parse()
-	// return
-	/*
 
-		// if loggingFormat == "text" {
-		log.SetLogger(klog.NewKlogr())
-		if loggingFormat == "json" {
-			// zapLog, _ := zap.NewProduction()
-			zl := zerolog.New(os.Stderr)
-			zl = zl.With().Caller().Timestamp().Logger()
-			klog.SetLogger(zerologr.New(&zl))
-			log.SetLogger(klogr.New())
-		}
-		setupLog.Info("custom message")
-		sa.Info("hey")
-	*/
 	if err := logging.Setup(loggingFormat); err != nil {
 		fmt.Println("failed to setup logger", err)
 		os.Exit(1)
